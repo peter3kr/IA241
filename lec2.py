9c3ff16f01f01a7bd802da0df6672ede869bc95b
EC2 Default User <ec2-user@ip-172-31-26-53.ec2.internal>
2021-01-31 19:51:05 +0000

    lec 2

---
+++ lec2.py
@@ -0,0 +1,28 @@
+"""
+This is a regional comment
+"""
+
+#print('Hello World') #This is a singe line comment
+
+#print(type("123"))
+
+# print("It's our second python class")
+
+# print('Hello' + ' World'
+
+my_str = 'hello world'
+
+print(my_str)
+
+my_str = 'second string'
+
+print(my_str)
+
+my_int = 2
+
+my_float = 2.0
+
+print(my_int + 3)
+print(my_int * 3)
+print(my_int ** 3)
+print(my_int + my_float)
