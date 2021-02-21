244ed4d4d7c02962a4cf51638dcc30aba5244453
EC2 Default User <ec2-user@ip-172-31-26-53.ec2.internal>
2021-02-21 20:19:33 +0000

    Lec5

---
+++ lec5.py
@@ -0,0 +1,38 @@
+"""
+lec 5 if statement
+"""
+
+#import this
+
+# a = [1,2,3]
+# b = [1,2,3]
+
+# print(list([1,2,3]))
+# print(list(a))
+# print(list(b))
+# print(a == b)
+# print(a is b)
+
+# x = None
+# print(id(x))
+# print(id(None))
+
+# print(x == None)
+# print(x is None)
+
+# y = []
+# print(y == None)
+# print(y is None)
+
+#print(not '0')
+
+# if 2>1:
+#     print('2>1')
+#     if 3>1:
+#         print('3>1')
+
+if 2<=1:
+    print('2<=1')
+else:
+    print('2>1')
+print('not in the if block')
