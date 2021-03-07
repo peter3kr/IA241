6eb0b8bcd43346f3cb8299e3cb089721f41d8b36
EC2 Default User <ec2-user@ip-172-31-26-53.ec2.internal>
2021-03-07 19:28:24 +0000

    Lab 7

---
+++ lec7.py
@@ -0,0 +1,24 @@
+"""
+Lec 7 while loop
+"""
+
+i = 5
+while i >= 0:
+    try:
+        print(1/(i-3))
+    except:
+        pass
+    i = i -1
+    
+    # if i ==3:
+    #     pass
+    
+    # print(i)
+    
+# try:
+#     print(1/0)
+# except ZeroDivisionError:
+#      print('Zero Division Error')
+     
+# except:
+#     print('Other Errors')
